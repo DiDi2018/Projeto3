2412d0193184a4b9c5004d0492495e9bc3ceb1b3
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-11-19 23:43:16 +0000

    testemouseover

---
+++ planta.js
@@ -0,0 +1,8 @@
+
+function mouseOverImage() {
+    document.getElementById("planta1").src = "images/MenuPlanta/planta1.1.png";
+}
+
+function mouseOutImage() {
+    document.getElementById("planta1").src = "images/MenuPlanta/planta1.png";
+}
