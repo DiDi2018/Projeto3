4ab1dda9c140ec717fe75fc94624bf320f6fa4ec
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-11-23 15:30:07 +0000

    cozinha quase td

--- cozinha.js
+++ cozinha.js
@@ -1,4 +1,17 @@
 function mouseOverImage() {
     var monstro2 = document.querySelector("#monstro2"); // works
-    monstro2.style.filter = "blur(5px)";
+    monstro2.src = "images/cozinha/Monstro1.1.png";
+}
+function mouseLeftImage() {
+    var monstro2 = document.querySelector("#monstro2"); // works
+    monstro2.src = "images/cozinha/Monstro1.png";
+}
+
+var carta=  document.getElementById('carta');
+carta.addEventListener("click", ShowLetter);
+
+function ShowLetter() {
+    text="<img id='papel' src='images/cozinha/papel.png'>";
+    document.getElementById('texto').innerHTML = text;
+
 }
