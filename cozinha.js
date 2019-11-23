3bcfc558050becf8d6f2a428213dd147b1d5590d
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-11-23 00:20:07 +0000

    coisas de bosta

---
+++ cozinha.js
@@ -0,0 +1,8 @@
+
+function mouseOverImage() {
+    document.getElementById("monstro2").style.filter = "blur(5px)";
+}
+
+function mouseOutImage() {
+    document.getElementById("monstro2").style.filter = "blur(0)";
+}
