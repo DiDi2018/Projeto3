b610ca5f6f6c516d7df05315d111494d90ce4a33
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-11-25 12:36:41 +0000

    cenas

--- JavaScript/planta.js
+++ JavaScript/planta.js
@@ -1,9 +1,28 @@
 let kitchen = document.querySelector("#kitchen");
+let livingroom = document.querySelector("#livingroom");
 
-function hover(){
-    kitchen.src = "images/MenuPlanta/planta1.1.png";
+function hoverkitchen(){
+    kitchen.src = "../images/MenuPlanta/planta1.1.png";
 }
-function nothover() {
-    kitchen.src = "images/MenuPlanta/planta1.png";
+function nothoverkitchen() {
+    kitchen.src = "../images/MenuPlanta/planta1.png";
 }
+function a(){
+    livingroom.src = "../images/MenuPlanta/planta2.1.png";
+}
+function b(){
+    livingroom.src = "../images/MenuPlanta/planta2.1.png";
+}
+function hoverlivingroom(){
+    livingroom.src = "../images/MenuPlanta/planta2.1.png";
+}
+function nothoverlivingroom() {
+    livingroom.src = "../images/MenuPlanta/planta2.png";
+}
+
+
+
+
+
+
 imageMapResize();
