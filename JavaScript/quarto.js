fecf8000ac284cc022646d05316ee71c077dad2a
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-11-25 12:07:12 +0000

    som cama e mais

--- JavaScript/quarto.js
+++ JavaScript/quarto.js
@@ -1,14 +1,9 @@
 //Som Lydia
-
 var audio_q1 = document.getElementById('audio_q1');
 
 function playaudio_q1() {
     if (audio_q1.paused) {
         audio_q1.play();
-    }
-    else {
-        audio1.pause();
-        audio1.currentTime = 0;
     }
 }
 
@@ -18,7 +13,7 @@ let text1Lydia = "This is the bedroom. It’s completely automatic so it does ev
 let i1 = 0;
 let speed = 50;
 let lydia= document.querySelector(".lydia");
-/*let audios = document.querySelectorAll("audio");*/
+
 
 function aparecerTexto(){
     if (i1 < text1Lydia.length){
@@ -33,4 +28,16 @@ lydia.addEventListener("click", aparecerTexto);
 //CAMA
 function alterar_cama(){
     document.getElementById('cama').src = "images/quarto/cama_azul.png"
+    audio_cama.play();
 }
+
+//SOM CAMA
+
+var audio_cama = document.getElementById('audio_cama');
+
+function playaudio_cama() {
+    if (audio_cama.paused) {
+        audio_cama.play();
+    }
+}
+
