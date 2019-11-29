3881b05ea40d077cc66e7ba639771e1dd00717aa
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-11-29 19:36:35 +0000

    update

--- JavaScript/quarto.js
+++ JavaScript/quarto.js
@@ -65,3 +65,20 @@ cama.addEventListener("click", audio_cama_f);
 audio_cama.addEventListener("ended", function(){
     cama.src = "images/quarto/cama.png";
 });
+
+//RELÓGIO
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('horas').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
