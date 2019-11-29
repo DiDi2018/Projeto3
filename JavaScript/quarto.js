af22744b0706e79b2d59a28ac776c9701d103755
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-11-29 19:11:08 +0000

    quarto_cama_update

--- JavaScript/quarto.js
+++ JavaScript/quarto.js
@@ -7,13 +7,13 @@ function playaudio_q1() {
     }
 }
 
+
 //texto lydia
 let text1Lydia = "This is the bedroom. It’s completely automatic so it does everything for you. " +
     "To open the curtains you just need to raise your eyebrows.";
 let i1 = 0;
 let speed = 50;
 let lydia= document.querySelector(".lydia");
-
 
 function aparecerTexto(){
     if (i1 < text1Lydia.length){
@@ -25,14 +25,43 @@ function aparecerTexto(){
 }
 lydia.addEventListener("click", aparecerTexto);
 
+
 //CAMA
-
 var audio_cama = document.getElementById('audio_cama');
+var cama = document.getElementById("cama");
 
-function alterar_cama() {
-        document.getElementById('cama').src = "images/quarto/cama_azul.png";
-        document.getElementById("cama").classList.add("cama_azul");
-        audio_cama.play();
+function mouseOverCama() {
+    var cama = document.getElementById("cama");
+    cama.src = "images/quarto/cama_azul.png";
+}
+function mouseLeftCama() {
+    var cama = document.getElementById("cama");
+    if(audio_cama.ended || audio_cama.paused){
+        cama.src = "images/quarto/cama.png";
+    }
 }
 
+function audio_cama_f() {
+    if (audio_cama.paused && audio_q1.paused) {
+        audio_cama.play();
+        document.getElementById("cama").classList.add("cama_azul");
+    } else if(audio_cama.paused && !(audio_q1.paused)) {
+        audio_q1.pause();
+        audio_q1.currentTime = 0;
+        audio_cama.play();
+        document.getElementById("cama").classList.add("cama_azul");
+    }
+    else {
+        audio_cama.pause();
+        audio_cama.currentTime = 0;
+        document.getElementById("cama").classList.remove("cama_azul");
+    }
+}
 
+cama.addEventListener("mouseenter", mouseOverCama);
+cama.addEventListener("mouseleave", mouseLeftCama);
+cama.addEventListener("click", audio_cama_f);
+
+audio_cama.addEventListener("ended", function(){
+    cama.src = "images/quarto/cama.png";
+});
