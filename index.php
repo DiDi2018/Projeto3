08b61278e37c4b7e9875f0f256560c94e01820ac
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-12-03 19:34:35 +0000

    inicio

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
     <div id="fundo_index" onclick="aparecer()"></div>
     <div id="popup">
         <img id='cruz_index' src='images/index/cruz.png' alt="cruz" onclick="desaparecer()">
-        <img class="fundopopup" src="images/index/fundo.png" height="2480" width="3508"/>
-        <img class="casa" src="images/index/Casa.png"/>
+        <img class="fundopopup" alt="""popup" src="images/index/fundo.png" height="2480" width="3508"/>
+        <img class="casa" alt="casa" src="images/index/Casa.png"/>
         <h1>GET A CHANCE OF WINING AN AMAZING HAPPY LIFE HOME!</h1>
 
         <form action="waiting.html" method="POST" >
             <h2>Enter now</h2>
             <input type="text" name="name" required><br>
             <label>
-                    <input type="submit" value="Submit">
+                    <input type="button" name="submeter" id="botao_submeter">
             </label>
         </form>
     </div>
--- sorteio.php
+++ sorteio.php
@@ -1,3 +1,7 @@
+<?php
+$nome = $_POST['name']
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +11,10 @@
 </head>
 <body>
 <header>
-    <div class="txt_processing">
-        <p>YOU WON</p>
+    <div class="txt_won">
+        <p>Hello <?php echo($nome) ?>!</p>
+        <p>We are happy to inform you that you've won the draw! Your visit to the Happy Life Home was scheduled for today. See you there.</p>
+        <a href="acenar.html" id="botao_start">START TOUR</a>
     </div>
 </header>
 </body>
