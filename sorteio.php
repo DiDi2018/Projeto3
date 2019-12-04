8f6d3f4f67baf3c9f82eee1cc113bc4021aec890
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-12-04 19:09:48 +0000

    coisas

--- sorteio.php
+++ sorteio.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$_SESSION['nome']=$_POST['name'];
 ?>
 
 <!DOCTYPE html>
---
+++ waiting.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$_SESSION['nome']=$_POST['name'];
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Processing</title>
+        <link rel="stylesheet" href="main.css">
+        <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <div id="quadrado_waiting"></div>
+        <div class="txt_processing">
+            <p>We are processing your submission...</p>
+        </div>
+        <img id="relogio_processing" src="images/relogio.png" alt="relogio">
+        <a href="sorteio.php" id="botao_start">START TOUR</a>
+    </header>
+    <script src="waiting.js"></script>
+    </body>
+    </html><?php
