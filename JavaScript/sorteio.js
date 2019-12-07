ca208eeb590aa41dfd89caeeb17e3e98b579417a
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-12-07 15:01:42 +0000

    chapéu sorteio

---
+++ JavaScript/sorteio.js
@@ -0,0 +1,22 @@
+var nrImagem = 0;
+var imagens = [];
+var refrescar = 1;
+
+// colocar aqui todas as imagens, seguindo a ordem numérica
+imagens[0] = "images/sorteio/chapeu.png";
+imagens[1] = "images/sorteio/chapeu1.png";
+
+
+function rodarImagens() {
+    document.querySelector(".sorteiochapeu").src = imagens[nrImagem++];
+
+    if (nrImagem >= imagens.length)
+        nrImagem = 0;
+
+    setTimeout("rodarImagens()", refrescar * 1000);
+}
+rodarImagens();
+
+function mouseOverChapeu() {
+
+}
--- sorteio.php
+++ sorteio.php
@@ -15,11 +15,18 @@ session_start();
 
 <header>
     <div class="txt_won">
-        <p>Hello <?php echo $_SESSION['nome'] ?>!</p>
         <p>We are happy to inform you that you've won the draw! Your visit to the Happy Life Home was scheduled for today. See you there.</p>
         <a href="cozinha.php" id="botao_start">START TOUR</a>
+        <div class="vencedor">
+        <p>Hello <?php echo $_SESSION['nome'] ?>!</p>
+        <img class="bilhete" src="images/sorteio/ticketblue.png" alt="ticket">
+        </div>
+        <img class="sorteiochapeu" alt="chapeu" src="images/sorteio/chapeu.png"/>
+
+
     </div>
 </header>
+<script src="JavaScript/sorteio.js"></script>
 
 </body>
 </html>
