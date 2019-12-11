c80770df2eeebab9265e05ae530dc0c5af2fc146
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-12-11 10:52:03 +0000

    gritos e boca

--- JavaScript/corredor.js
+++ JavaScript/corredor.js
@@ -1,30 +1,28 @@
-document.addEventListener("click",function(){
-    let image1 = document.querySelector(".hallway:nth-child(1)").style.transform;
-    let value1 = image1.match(/translateZ\(([-]?)[0-9]{1,4}px\)/);
-    let value = parseInt(value1[0].slice(11,value1[0].length - 3));
-
-    if(value < -500){
-        for(let i=1; i<17; i++){
-            let image = document.querySelector(`.hallway:nth-child(${i})`);
-            let imageCSS = image.style.transform;
-            let valueIni = imageCSS.match(/translateZ\(([-]?)[0-9]{1,4}px\)/);
-            let value = parseInt(valueIni[0].slice(11,valueIni[0].length - 3)) + 5;
-            value.toString();
-
-            image.style.transform = imageCSS.replace(/translateZ\(([-]?)[0-9]{1,4}px\)/,`translateZ(${value}px)`);
-        }
-    }
-
-});
-
-//CÂMARA
-
+//document.addEventListener("click", aproximar);
 var capture;
 var tracker;
 var w = 640,
     h = 480;
-var positions;
+var gritos=document.getElementById("grito");
+function aproximar() {
+    let image1 = document.querySelector(".hallway:nth-child(1)").style.transform;
+    let value1 = image1.match(/translateZ\(([-]?)[0-9]{1,4}px\)/);
+    let value = parseInt(value1[0].slice(11, value1[0].length - 3));
 
+    if (value < -500) {
+        for (let i = 1; i < 17; i++) {
+            let image = document.querySelector(`.hallway:nth-child(${i})`);
+            let imageCSS = image.style.transform;
+            let valueIni = imageCSS.match(/translateZ\(([-]?)[0-9]{1,4}px\)/);
+            let value = parseInt(valueIni[0].slice(11, valueIni[0].length - 3)) + 25;
+            value.toString();
+
+            image.style.transform = imageCSS.replace(/translateZ\(([-]?)[0-9]{1,4}px\)/, `translateZ(${value}px)`);
+        }
+    }
+}
+
+//CÂMARA
 function setup() {
     capture = createCapture({
         audio: false,
@@ -32,7 +30,7 @@ function setup() {
             width: w,
             height: h
         }
-    }, function() {
+    }, function () {
         console.log('capture ready.')
     });
     capture.elt.setAttribute('playsinline', '');
@@ -45,30 +43,27 @@ function setup() {
     tracker = new clm.tracker();
     tracker.init();
     tracker.start(capture.elt);
-    frameRate(20);
 }
 
 function draw() {
-    positions = tracker.getCurrentPosition();
-    /*
-    image(capture, 0, 0, w, h);
-    noFill();
-    stroke(255);
-    beginShape();
-    for (let i = 0; i < positions.length; i++) {
-        vertex(positions[i][0], positions[i][1]);
-    }
-    endShape();
-    for (let i = 0; i < positions.length; i++) {
-        fill(map(i, 0, positions.length, 0, 360), 50, 100);
-        // ellipse(positions[i][0], positions[i][1], 2, 2);
-        text(i, positions[i][0], positions[i][1]);
-    }
-    */
+    // image(capture, 0, 0, w, h);
+    var positions = tracker.getCurrentPosition();
 
+    //distância entre sobrancelha e olho
     if (positions.length > 0) {
-        var mouthUp = createVector();
-        var mouthLow = createVector();
+        var MouthTop = createVector(positions[58][0], positions[58][1]);
+        var MouthBottom = createVector(positions[59][0], positions[59][1]);
+        var boca = MouthTop.dist(MouthBottom);
+    }
+
+    if (boca >= 26) {
+        aproximar();
+        gritos.play();
+
+    }
+    else{
+        gritos.pause();
+
 
     }
 }
