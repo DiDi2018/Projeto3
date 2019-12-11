96f95f5744bedeecaab31dec49318bcb4d70e3d3
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2019-12-11 14:14:11 +0000

    not working

--- JavaScript/corredor.js
+++ JavaScript/corredor.js
@@ -1,9 +1,13 @@
 //document.addEventListener("click", aproximar);
+var tv = document.getElementById("tv");
+var audio_ruido = document.getElementById('audio_ruido');
+
 var capture;
 var tracker;
 var w = 640,
     h = 480;
 var gritos=document.getElementById("grito");
+
 function aproximar() {
     let image1 = document.querySelector(".hallway:nth-child(1)").style.transform;
     let value1 = image1.match(/translateZ\(([-]?)[0-9]{1,4}px\)/);
@@ -19,6 +23,9 @@ function aproximar() {
 
             image.style.transform = imageCSS.replace(/translateZ\(([-]?)[0-9]{1,4}px\)/, `translateZ(${value}px)`);
         }
+    }
+    if(value === -700){
+        setTimeout(mudar, 500);
     }
 }
 
@@ -67,3 +74,57 @@ function draw() {
 
     }
 }
+
+//RUIDO
+function mudar() {
+    setTimeout(ruidar, 1000);
+    setTimeout(desafio, 4000);
+}
+
+function ruidar() {
+    tv.src = "images/corredor/tv_ruido.png";
+    audio_ruido.play();
+    gritos.pause();
+}
+
+function desafio() {
+    tv.src = "images/corredor/tv.png";
+    audio_ruido.pause();
+    gritos.pause();
+}
+
+//mover letras
+var dragMe = document.getElementById("letra_A"),
+    /* o x inicial do drag*/
+    dragOfX = 15;
+    /* o y inicial do drag */
+    dragOfY = 15;
+
+/* ao segurar o elemento */
+function dragStart(e) {
+    /* define o x inicial do drag */
+    dragOfX = e.pageX - dragMe.offsetLeft;
+    /* define o y inicial do drag */
+    dragOfY = e.pageY - dragMe.offsetTop;
+
+    /* adiciona os eventos */
+    addEventListener("mousemove", dragMove);
+    addEventListener("mouseup", dragEnd);
+}
+
+/* ao ser arrastado */
+function dragMove(e) {
+    /* atualiza a posição do elemento */
+    dragMe.style.left = (e.pageX - dragOfX) + 'px';
+    dragMe.style.top = (e.pageY - dragOfY) + 'px';
+}
+
+/* ao terminar o drag */
+function dragEnd() {
+    /* remove os eventos */
+    removeEventListener("mousemove", dragMove);
+    removeEventListener("mouseup", dragEnd);
+}
+
+/* adiciona o evento que começa o drag */
+dragMe.addEventListener("mousedown", dragStart);
