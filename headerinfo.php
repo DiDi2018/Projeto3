e5a04901a8fc6ac871573787fcdb6fe8b458c90c
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2019-12-13 00:10:36 +0000

    Create headerinfo.php

---
+++ headerinfo.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sorteio</title>
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<div id="triangulo1"></div>
+<div id="triangulo2"></div>
+<header>
+
+</header>
+</body>
+</html>
+
