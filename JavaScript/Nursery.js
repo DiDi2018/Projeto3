35b398f9af7d3d6837bea19a14a0cd379343890c
DiDi2018 <39709254+DiDi2018@users.noreply.github.com>
2019-12-14 15:52:13 +0000

    nursery

--- JavaScript/Nursery.js
+++ JavaScript/Nursery.js
@@ -8,22 +8,19 @@ imagens[1] = "images/nursery/girafa_ruido.png";
 
 
 function rodarImagem() {
-
-document.querySelector('girafa').setAttribute('src', imagens[nrImagem++])
-
+    document.querySelector('#girafa').setAttribute('src', imagens[nrImagem++]);
     if (nrImagem >= imagens.length)
         nrImagem = 0;
-
     setTimeout("rodarImagem()", refrescar * 500);
 }
+
 rodarImagem();
 
-
-
-let text1Lydia = "This is the bedroom. It’s completely automatic so it does everything for you. " +
-    "To open the curtains you just need to raise your eyebrows.";
-let i1=0;
-let speed = 50;
+let audio = document.querySelectorAll("audio");
+let text1Lydia = "This room is the heart of the house. Whenever you feel like escaping reality " +
+                "you can do it without actually leaving. You will feel like you're in another world!";
+let i1 = 0;
+let speed = 45;
 
 function aparecerTexto() {
     if (i1 < text1Lydia.length) {
@@ -31,6 +28,23 @@ function aparecerTexto() {
         i1++;
         setTimeout(aparecerTexto, speed);
     }
+    else {
+        removerTexto();
+    }
 }
-//audio_q1.play();
+
+function removerTexto(){
+    if(i1>0 && audio[0].ended){
+        let tmp = document.querySelector(".lydiaTexto p").innerHTML;
+        tmp = tmp.slice(0,i1 -1);
+        document.querySelector(".lydiaTexto p").innerHTML = tmp;
+        i1 = i1 - 1;
+        setTimeout(removerTexto, 30);
+    }
+    if(!audio[0].ended){
+        setTimeout(removerTexto, 30);
+    }
+}
+
+audio[0].play();
 aparecerTexto();
