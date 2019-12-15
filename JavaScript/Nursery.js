ea6983bde0d0f771c3393681abbbce12d0eb2ae8
DiDi2018 <39709254+DiDi2018@users.noreply.github.com>
2019-12-15 18:52:32 +0000

    nursery

--- JavaScript/Nursery.js
+++ JavaScript/Nursery.js
@@ -7,14 +7,14 @@ imagens[0] = "images/nursery/girafa.png";
 imagens[1] = "images/nursery/girafa_ruido.png";
 
 
-function rodarImagem() {
+/*function rodarImagem() {
     document.querySelector('#girafa').setAttribute('src', imagens[nrImagem++]);
     if (nrImagem >= imagens.length)
         nrImagem = 0;
     setTimeout("rodarImagem()", refrescar * 500);
 }
 
-rodarImagem();
+rodarImagem();*/
 
 let audio = document.querySelectorAll("audio");
 let text1Lydia = "This room is the heart of the house. Whenever you feel like escaping reality " +
@@ -34,17 +34,32 @@ function aparecerTexto() {
 }
 
 function removerTexto(){
-    if(i1>0 && audio[0].ended){
+    if(i1>0 && audio[1].ended){
         let tmp = document.querySelector(".lydiaTexto p").innerHTML;
         tmp = tmp.slice(0,i1 -1);
         document.querySelector(".lydiaTexto p").innerHTML = tmp;
         i1 = i1 - 1;
         setTimeout(removerTexto, 30);
     }
-    if(!audio[0].ended){
+
+    else if(!audio[1].ended){
         setTimeout(removerTexto, 30);
+    }
+
+    if(i1 <= 0){
+        setTimeout(function(){
+            document.querySelector("#ruido").style.display = "block";
+            document.querySelector('a-scene').style.display = "none";
+            document.getElementById("audio_ruido").play();
+        },10000);
+        setTimeout(function(){
+            document.querySelector("#ruido").style.display = "none";
+            document.getElementById("audio_ruido").pause();
+        },12000);
     }
 }
 
-audio[0].play();
-aparecerTexto();
+document.querySelector('a-scene').addEventListener('loaded', function () {
+    audio[1].play();
+    aparecerTexto();
+});
