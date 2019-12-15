c9a713809bf93a78a071ea0b8776d7aa8af207bb
DiDi2018 <39709254+DiDi2018@users.noreply.github.com>
2019-12-15 23:06:50 +0000

    sfsdfs

--- sorteio.php
+++ sorteio.php
@@ -11,7 +11,7 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,700,700i&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body style="overflow: hidden">
 
 <header>
     <img class="sobre" src="images/sobre.png" alt=""/>
