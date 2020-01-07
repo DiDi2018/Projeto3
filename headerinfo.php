2428cc2a476be2c2da8310643a3b809f7ae611c3
DiDi2018 <39709254+DiDi2018@users.noreply.github.com>
2020-01-07 13:30:59 +0000

    help com tamanho correto

--- headerinfo.php
+++ headerinfo.php
@@ -3,11 +3,11 @@
 <div class="instrucoes">
     <div class="instrucoestexto">
         <h2>Are you lost?</h2>
-        <p>Explore the room by clicking on all technology-related objects that turn blue when the mouse is over them.
-            Only
-            after exploring the room and facing the challenges proposed by Lydia you'll be able to go to the next level.
-            Good luck! </p>
-
+        <p>
+            <br>Explore the room by clicking on all technology-related objects that turn blue when the mouse is over them.
+            Only after exploring the room and facing the challenges proposed by Lydia you'll be able to go to the next level.
+            <br>Good luck!
+        </p>
     </div>
     <img class="postit" src="images/header/postit.png" alt=""/>
     <div class="fundo"></div>
